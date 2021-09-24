2bc67e72e212e35721a227b7cd2bbbc97f1e23bf
Devil <ysodevilo@163.com>
2021-09-24 13:56:08 +0800

    提交下

--- src/main/java/com/devil/guide/arithmetic/BigFileSort.java
+++ src/main/java/com/devil/guide/arithmetic/BigFileSort.java
@@ -1,6 +1,10 @@
 package com.devil.guide.arithmetic;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.*;
 
@@ -14,6 +18,7 @@ import java.util.concurrent.*;
  * 假设都是 ascii 码
  * 整个文件大小最大的情况为 200000000 * 255 = 51000000000b > 48637GB 超过内存大小，考虑多线程，以及多文件处理
  * <p>
+ *     2147483647
  * 按 ascii 码以及字符长度区间分桶 桶的计算大小为 256 * 256 = 65536
  * 上面按 65536 分桶不合理 最坏的情况，假如刚好2亿行都分到一个文件区间，那这个桶就有所有数据，还是无法在内存中排序
  * <p>
@@ -27,32 +32,21 @@ import java.util.concurrent.*;
  * @since 2021/9/16
  */
 public class BigFileSort {
-    /**
-     * 文件个数
-     */
-    private final int fileCount;
-    /**
-     * 小文件行数
-     */
-    private final int fileLines;
+    /** 核数 */
+    private int coreSize = 16;
+    /** 假设一个文件按 2g 2147483648 / 256 = 8388608 */
+    private int fileLines = 8000000;
+    /** 文件个数 200000000 / 8000000 = 25 */
+    private int fileCount = 25;
 
-    private final ThreadPoolExecutor corePool;
-    /**
-     * 文件名和文件的映射
-     */
-    private final Map<String, SplitFileReader> fileMap;
+    /** 排序和小文件写入的线程池 */
+    private ThreadPoolExecutor corePool;
+    /** 文件名和文件的映射 */
+    private Map<String, NioFile> fileMap;
+    /** 根据字符串排序的链表 */
+    private BlockingQueue<List<String>> writeQueue;
 
-    private final BlockingQueue<List<String>> writeQueue;
-
-    /**
-     * @param coreSize  核数 单位个
-     * @param memory    内存大小 单位B
-     * @param totalCount 总行数
-     */
-    BigFileSort(int coreSize, int memory, int totalCount) {
-        // 内存 / 核数 则为每个核能操作的内存大小  再除以每行的最大大小 则为每个小文件的行数
-        this.fileLines = memory/ coreSize / 256;
-        this.fileCount = (int) Math.ceil((double) totalCount / fileLines);
+    BigFileSort() {
         this.corePool = new ThreadPoolExecutor(coreSize, coreSize, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(coreSize));
         this.fileMap = new HashMap<>();
         this.writeQueue = new LinkedBlockingQueue<>(coreSize);
@@ -69,7 +63,7 @@ public class BigFileSort {
             LinkedList<FileValueNode> link = initLink();
 
             // 3. 启动线程进行消费写入最终文件的操作
-            new Thread(new WriteTask(outFileName, writeQueue)).start();
+            new Thread(new WriteTask(new NioFile(outFileName), writeQueue)).start();
 
             // 4. 取链表第一个值，然后从对应文件再读取一个值，放到对应位置 直到数全部读取完
             writeToOut(link);
@@ -80,60 +74,54 @@ public class BigFileSort {
     }
 
     private void readInFile(String fileName, CountDownLatch latch) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        List<String> lines = new ArrayList<>();
-        String line = reader.readLine();
+        NioFile nioFile = new NioFile(fileName);
+        List<String> lines = new ArrayList<>(); // 每个小文件的数据
         int fileIdx = 1;
+        String line = nioFile.nextLine();
         while (line != null) {
             lines.add(line);
-            line = reader.readLine();
-            // 任务达到一定值，往任务队列里面丢
             if (lines.size() >= fileLines) {
-                String smallFileName = fileIdx + ".txt";
-                corePool.submit(new SingleFileSortTask(smallFileName, lines, latch));
-                fileMap.put(fileName, new SplitFileReader(smallFileName));
+                NioFile sFile = new NioFile(fileIdx + ".txt");
+                corePool.submit(new SingleFileSortTask(sFile, lines, latch));
+                fileMap.put(fileName, sFile);
                 lines = new ArrayList<>();
                 fileIdx++;
             }
+            line = nioFile.nextLine();
         }
         if (lines.size() > 0) {
-            String smallFileName = fileIdx + ".txt";
-            corePool.submit(new SingleFileSortTask(smallFileName, lines, latch));
-            fileMap.put(fileName, new SplitFileReader(smallFileName));
+            NioFile sFile = new NioFile(fileIdx + ".txt");
+            corePool.submit(new SingleFileSortTask(sFile, lines, latch));
+            fileMap.put(fileName, sFile);
         }
     }
 
-    private LinkedList<FileValueNode> initLink() {
+    private LinkedList<FileValueNode> initLink() throws IOException {
         LinkedList<FileValueNode> link = new LinkedList<>();
-        for (Map.Entry<String, SplitFileReader> entry : this.fileMap.entrySet()) {
-            SplitFileReader splitFileReader = entry.getValue();
-            splitFileReader.init();
-            String fline = splitFileReader.nextLine();
-            // 将值放到一个合适位置
-            if (link.size() <= 0) {
-                link.add(new FileValueNode(entry.getKey(), fline));
-            } else {
-                addToPos(entry.getKey(), fline, link);
-            }
+        for (Map.Entry<String, NioFile> entry : this.fileMap.entrySet()) {
+            NioFile nioFile = entry.getValue();
+            String fline = nioFile.nextLine();
+            addToPos(new FileValueNode(entry.getKey(), fline), link);
         }
         return link;
     }
 
-    private void writeToOut(LinkedList<FileValueNode> link) {
+    private void writeToOut(LinkedList<FileValueNode> link) throws IOException {
         List<String> lines = new ArrayList<>();
         while (link.size() > 0) {
+            // 将头部最小的元素添加到列表
             FileValueNode first = link.pollFirst();
             lines.add(first.getLine());
+            // 超过一定量就往文件写入
             if (lines.size() >= fileLines) {
-                // 往文件写入
                 writeQueue.add(lines);
                 lines = new ArrayList<>();
             }
-            // 从对应文件补充一个数，直到文件读取完
-            SplitFileReader splitFileReader = fileMap.get(first.getFileName());
-            String line = splitFileReader.nextLine();
+            // 从对应文件补充一个数 如果这个文件读取完了就跳过
+            NioFile nioFile = fileMap.get(first.getFileName());
+            String line = nioFile.nextLine();
             if (line != null && line.length() > 0) {
-                addToPos(first.getFileName(), line, link);
+                addToPos(new FileValueNode(first.getFileName(), line), link);
             }
         }
         if (lines.size() > 0 ){
@@ -141,14 +129,21 @@ public class BigFileSort {
         }
     }
 
-    private void addToPos(String fileName, String line, LinkedList<FileValueNode> link) {
-        int pos = link.size();
-        for (int i = 0; i < link.size(); i++) {
-            if (getStringScore(link.get(i).getLine()) > getStringScore(line)) {
-                pos = i;
+    /**
+     * 在对应位置放入节点
+     */
+    private void addToPos(FileValueNode node, LinkedList<FileValueNode> link) {
+        if (link.size() <= 0) {
+            link.add(node);
+        } else {
+            int pos = link.size();
+            for (int i = 0; i < link.size(); i++) {
+                if (getStringScore(link.get(i).getLine()) > getStringScore(node.getLine())) {
+                    pos = i;
+                }
             }
+            link.add(pos, node);
         }
-        link.add(pos, new FileValueNode(fileName, line));
     }
 
     /**
@@ -156,37 +151,35 @@ public class BigFileSort {
      */
     static class SingleFileSortTask implements Runnable {
 
-        private final String fileName;
+        private final NioFile nioFile;
 
         private final List<String> lines;
 
         private final CountDownLatch latch;
 
-        SingleFileSortTask(String fileName, List<String> lines, CountDownLatch latch) {
-            this.fileName = fileName;
+        SingleFileSortTask(NioFile nioFile, List<String> lines, CountDownLatch latch) {
+            this.nioFile = nioFile;
             this.lines = lines;
             this.latch = latch;
         }
 
         @Override
         public void run() {
-            // 排序
-            lines.sort(new Comparator<String>() {
-                @Override
-                public int compare(String o1, String o2) {
-                    return getStringScore(o1) - getStringScore(o2);
-                }
-            });
-            // 往文件写
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-                for (String line : lines) {
-                    writer.write(line);
-                }
-                writer.flush();
+            try {
+                // 排序
+                lines.sort(new Comparator<String>() {
+                    @Override
+                    public int compare(String o1, String o2) {
+                        return getStringScore(o1) - getStringScore(o2);
+                    }
+                });
+                // 往文件写
+                nioFile.write(lines);
+                // 计数器减少
+                latch.countDown();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            latch.countDown();
         }
     }
 
@@ -195,28 +188,21 @@ public class BigFileSort {
      */
     static class WriteTask implements Runnable {
 
-        BufferedWriter writer;
+        NioFile nioFile;
 
         BlockingQueue<List<String>> queue;
 
-        WriteTask(String fileName, BlockingQueue<List<String>> queue) {
-            try {
-                this.writer = new BufferedWriter(new FileWriter(fileName));
-                this.queue = queue;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        WriteTask(NioFile nioFile, BlockingQueue<List<String>> queue) {
+            this.nioFile = nioFile;
+            this.queue = queue;
         }
 
         @Override
         public void run() {
             while (true) {
                 try {
-                    List<String> take = queue.take();
-                    for (String line : take) {
-                        writer.write(line);
-                    }
-                    writer.flush();
+                    List<String> lines = queue.take();
+                    nioFile.write(lines);
                 } catch (InterruptedException | IOException e) {
                     e.printStackTrace();
                 }
@@ -226,33 +212,69 @@ public class BigFileSort {
     }
 
     /**
-     * 文件读取的工具
+     * 文件读写工具
      */
-    static class SplitFileReader {
+    static class NioFile {
 
-        private final String fileName;
+        private RandomAccessFile randomAccessFile;
 
-        private BufferedReader reader;
+        private FileChannel channel;
 
-        SplitFileReader(String fileName) {
-            this.fileName = fileName;
+        private ByteBuffer buffer = ByteBuffer.allocate(1024 * 1024); // 每次读取文件的缓冲
+
+        private ByteBuffer lineBuffer = ByteBuffer.allocate(256); // 存每一行的数据
+
+        private int bytesRead = -100;
+
+        NioFile(String fileName) throws IOException {
+            this.randomAccessFile = new RandomAccessFile(fileName, "rw");
+            this.channel = randomAccessFile.getChannel();
+
         }
 
-        public void init() {
-            try {
-                this.reader = new BufferedReader(new FileReader(fileName));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+        public String nextLine() throws IOException {
+            if (bytesRead == -100) {
+                bytesRead = channel.read(buffer);
+                if (bytesRead == -1) {
+                    String line = StandardCharsets.UTF_8.decode(lineBuffer).toString();
+                    lineBuffer.clear(); // 清理下次直接返回 null
+                    return line.trim().equals("") ? null : line; // 文件已经读取完
+                }
+                buffer.flip(); // 切换到读
             }
+            while (buffer.hasRemaining()) {
+                byte b = buffer.get();
+                if (b == 10 || b == 13) { // 换行
+                    lineBuffer.flip();
+                    String line = StandardCharsets.UTF_8.decode(lineBuffer).toString();
+                    lineBuffer.clear();
+                    return line; // 读取到一行数据 直接返回
+                } else {
+                    lineBuffer.put(b);
+                }
+            }
+            buffer.clear();
+            bytesRead = -100;
+            return nextLine(); // 如果没有在while返回那么说明缓冲区内数据不满一行，需要从下一数据中获取
         }
 
-        public String nextLine() {
-            try {
-                return reader.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
+        public void write(List<String> lines) throws IOException {
+            buffer.clear();
+            // 4000行一次写入
+            int size = 0;
+            for (String line : lines) {
+                buffer.put(line.getBytes());
+                size++;
+                while (size >= 4000) {
+                    channel.write(buffer);
+                    buffer.clear();
+                    size = 0;
+                }
             }
-            return null;
+            if (size > 0) {
+                channel.write(buffer);
+                buffer.clear();
+            }
         }
 
     }
